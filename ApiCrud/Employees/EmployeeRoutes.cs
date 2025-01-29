ede5a0b45c25f8a3623ec3854dc563f5af8014e5
Hora-keven <kevensantos993@gmail.com>
2025-01-29 10:37:09 -0300

    feat: new method and another corrected

--- ApiCrud/Employees/EmployeeRoutes.cs
+++ ApiCrud/Employees/EmployeeRoutes.cs
@@ -11,17 +11,29 @@ namespace ApiCrud.Employees
     {
         public static void AddEmployeeRoutes(this WebApplication app){
             var route = app.MapGroup("employee");
-            
+
             route.MapPost("",async (EmployeeRequest req, AppDbContext context)=>{
                 var newEmployee = new Employee(req.Name);
+                var existEmployee = await context.Employees.AnyAsync(employee=>employee.Name == req.Name);
+                if (existEmployee) return Results.Conflict("Employee already exist");
                 await context.Employees.AddAsync(newEmployee);
                 await context.SaveChangesAsync();
                 return Results.Created();
             });
 
             route.MapGet("", async(AppDbContext context)=>{
-                var employees = await context.Employees.ToListAsync();
+                var employees = await context.Employees.Where(employee=>employee.IsActive).ToListAsync();
                 return Results.Ok(employees);
+            });
+
+            route.MapPut("{id:guid}", async(Guid id, AppDbContext context, UpdateEmployeeRequest req)=>{
+                var employee = await context.Employees.SingleOrDefaultAsync(employee=>employee.Id == id);
+                if(employee == null) return Results.NotFound();
+                employee.updateName(req.Name);
+
+                await context.SaveChangesAsync();
+                return Results.Ok(employee);
+
             });
         }
     }
