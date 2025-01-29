5dad5abcffe30451be62530d5ed37543f7ad307a
Hora-keven <kevensantos993@gmail.com>
2025-01-29 09:26:25 -0300

    feat:  connection with database

---
+++ ApiCrud/Data/AppDbContext.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ApiCrud.Employees;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiCrud.Data
+{
+    public class AppDbContext: DbContext
+    {
+        public DbSet<Employee> Employees {get; set;}
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=database.sqlite");
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
